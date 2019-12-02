5b899aa40a1f1328b75d7b2aa81478e9ae0644c2
Artemiy <aaegorof@gmail.com>
2019-12-02 13:15:36 +0300

    Change steps to get from state.

--- src/components/Steps/index.js
+++ src/components/Steps/index.js
@@ -1,32 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
 import handleViewport from "react-in-viewport";
 import "./style.scss";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Iphone from "./Iphone";
-import { connect } from "react-redux";
-import { changeCurrentStep } from "../../actions";
 
 const stepss = [Step1, Step2];
 
-const s2p = ({ steps }) => ({
-  steps: steps
-});
-const d2p = dispatch => ({
-  changeCurrentStep: stepNum => dispatch(changeCurrentStep(stepNum))
-});
 
 const stepConfig = {
   offset: [-20, -20],
   threshhold: 0.5
 };
 
+const stepViewportOpt = {
+  rootMargin: `${stepConfig.offset[0]}% 0px ${
+      stepConfig.offset[1]
+  }% 0px`,
+  threshold: stepConfig.threshhold
+}
+
 const Steps = props => {
-  const { changeCurrentStep } = props;
+  const [curStep, changeStep] = useState(0)
+
   return (
     <section className="steps-wrap full-height">
-      <Iphone step={props.steps.currentStep} />
+      <Iphone step={curStep} />
       {/*<Parallax pages={3} >*/}
       {/*  <ParallaxLayer offset={0} speed={-1} className="centered phone">*/}
       {/*    <img src={phone} />*/}
@@ -36,21 +36,16 @@ const Steps = props => {
       {/*  <ParallaxLayer offset={2} speed={0.1} className="bg-grey">*/}
       {/*  </ParallaxLayer>*/}
       {/*</Parallax>*/}
-      {stepss.map((St, i) => {
+      {stepss.map((Component, i) => {
         const StepBlock = handleViewport(
-          props => <St step={i} stepConfig={stepConfig} {...props} />,
-          {
-            rootMargin: `${stepConfig.offset[0]}% 0px ${
-              stepConfig.offset[1]
-            }% 0px`,
-            threshold: stepConfig.threshhold
-          }
+          props => <Component step={i} stepConfig={stepConfig} {...props} />,
+          stepViewportOpt
         );
 
         return (
           <StepBlock
             key={i}
-            onEnterViewport={() => changeCurrentStep(i)}
+            onEnterViewport={() => changeStep(i)}
             onLeaveViewport={() => console.log("leave")}
           />
         );
@@ -59,7 +54,4 @@ const Steps = props => {
   );
 };
 
-export default connect(
-  s2p,
-  d2p
-)(Steps);
+export default Steps;
--- src/components/Top/index.js
+++ src/components/Top/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./style.scss";
+import "../../styles/animations.scss";
 import mask from "../../img/main/main-mask.svg";
 import logo from "../../img/logo.svg";
 import megafon from "../../img/main/rupor.svg";
 import lupa from "../../img/main/lupa.svg";
 import { animated, useSpring } from "react-spring";
 
-
 const trans = num => (x, y) => {
-  const xx = num < 30 ? num : -num
-  const yy = num < 30 ? num : -num
- return `translate3d(${x / xx}px,${y / yy}px,0)`;
-}
+  const xx = num < 30 ? num : -num;
+  const yy = num < 30 ? num : -num;
+  return `translate3d(${x / xx}px,${y / yy}px,0)`;
+};
 
 const Top = () => {
   const [parEl, setParEl] = useSpring(() => ({
@@ -29,16 +29,30 @@ const Top = () => {
         })
       }
     >
-      <animated.img
-        src={megafon}
-        className="fl-icon icon-left-2"
-        style={{ transform: parEl.xy.interpolate(trans( Math.random() * 20 + 22)) }} //min 22 and max is 42
-      />
-      <animated.img
-          src={lupa}
-          className="fl-icon icon-right-2"
-          style={{ transform: parEl.xy.interpolate(trans( Math.random() * 20 + 22)) }}
-      />
+
+      <div className="container relative">
+
+        <animated.div
+            className="fl-icon double-rotating"
+            style={{
+              transform: parEl.xy.interpolate(trans(Math.random() * 20 + 22)),
+              left: 0,
+              top: "22%"
+            }} //min 22 and max is 42
+        >
+          <img src={megafon} />
+        </animated.div>
+
+        <div className="double-rotating fl-icon" style={{ right: 0, top: "32%" }}>
+          <animated.img
+              src={lupa}
+              style={{
+                transform: parEl.xy.interpolate(trans(Math.random() * 20 + 22))
+              }}
+          />
+        </div>
+      </div>
+
     </div>
   );
 };
